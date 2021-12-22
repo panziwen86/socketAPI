81c26fe977262f8f9af61d7bf313e4f0cb455ed1
panziwen86 <panziwen86@126.com>
2021-12-22 10:13:22 +0800

    修改GetTick接口

--- common.h
+++ common.h
@@ -1,6 +1,18 @@
 #include<stdio.h>
+#include<time.h>
 
 inline long long GetTick(void)
 {
-    return 0;
+#ifdef _WIN32
+	//return GetTickCount64();
+	return GetTickCount();
+#elif defined MAC_OS
+    struct timeval us;
+    gettimeofday(&us, NULL);
+    return (us.tv_sec*1000 + us.tv_usec/1000);
+#else
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return ((long long)ts.tv_sec * 1000LL + ts.tv_nsec/(1000 * 1000));
+#endif
 }
